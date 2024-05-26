2a255540cedf0c83ef866e5eef23ab9f4db3ba37
Sweelam <sweelam.fcih2014@gmail.com>
2024-05-26 07:07:29 +0400

    Use spring reactive mongo

--- src/main/java/com/sweelam/Main.java
+++ src/main/java/com/sweelam/Main.java
@@ -3,8 +3,10 @@ package com.sweelam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class Main {
     public static void main(String[] args) {
 
